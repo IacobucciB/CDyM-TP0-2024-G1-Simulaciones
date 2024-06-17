a01b896d98b0659b4cc3f3df0c291f988828282c
Iacobucci Juan B. <link00222@gmail.com>
2024-06-17 16:35:21 -0300

    initial commit, proteus parts

---
+++ tp0/main.c
@@ -0,0 +1,18 @@
+/*
+ * tp0.c
+ *
+ * Created: 17/6/2024 16:00:53
+ * Author : admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
