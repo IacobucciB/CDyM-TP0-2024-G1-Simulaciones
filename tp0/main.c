d778d49b5ce26eb6f19c13cafce83b6145910b85
IacobucciB <link00222@gmail.com>
2025-08-14 13:57:02 -0300

    done

--- tp0/main.c
+++ tp0/main.c
@@ -2,11 +2,22 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
-
 int main(void)
-{	
-	while(1) {
+{
 
-    }
+	DDRD = 0xFF;
+	PORTD = 0x00;
+
+	DDRB = 0x00;
+	PORTB = 0xFF;
+	
+	while(1)
+	{
+		if (PINB)
+		{
+			PIND = PINB;
+		}
+	}
+	return 0;
 }
 
